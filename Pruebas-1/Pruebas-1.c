cf55d4705d258fa6231640a55c3acb88b73660a5
Basilio Matijasevich, Agustín <81303198+Agustin-Basilio-Matijasevich@users.noreply.github.com>
2021-04-26 14:21:18 -0300

    creacion proyecto primer examen

--- Pruebas-1/Pruebas-1.c
+++ Pruebas-1/Pruebas-1.c
@@ -1,26 +1,8 @@
 #include <stdio.h>
 
-#define iteracion 5
-
 int main()
 {
-	int cont, aux;
-	float suma;
-	cont = 0; suma = 0;
-
-	while (cont < iteracion)
-	{
-		printf("Ingrese un numero: ");
-		scanf_s("%i", &aux);
-		printf("Usted ingreso: %i\n", aux);
-		suma += aux;
-		cont++;
-	}
-
-	printf("La suma total es: %.0f\n", suma);
-	printf("El promedio es: %.2f\n", suma / iteracion);
-
-	system("Pause");
+	printf("Hello world");
 
 	return 0;
 }
