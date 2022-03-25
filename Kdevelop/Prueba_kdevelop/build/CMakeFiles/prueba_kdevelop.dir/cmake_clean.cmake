1721f64f447e5347b4a9d56cfb25382410f52792
Agustin-Basilio-Matijasevich <agustinbasiliomatijasevich@gmail.com>
2022-03-25 13:16:13 -0300

    Prueba de Kdevelop para C

---
+++ Kdevelop/Prueba_kdevelop/build/CMakeFiles/prueba_kdevelop.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prueba_kdevelop.dir/main.c.o"
+  "prueba_kdevelop"
+  "prueba_kdevelop.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/prueba_kdevelop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
